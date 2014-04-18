cd03073f8793c31261d19ddc832ba74be0bd57ba
unknown <lawsonch@LAWSONCH4.americas.hpqcorp.net>
2014-04-18 13:54:19 -0700

    Testing hard-coded election status

--- app/models/election.js
+++ app/models/election.js
@@ -5,6 +5,21 @@ var mongoose = require("mongoose"),
 
 module.exports = mongoose.model('Election', new Schema({
     election_id   : ObjectId
-    , status      : { type: String, required: true }
+    , status      : { type: String, default: "Pre-election", required: true }
     , mod_date    : { type: Date, default: Date.now }
 }));
+
+// hard code the election status
+var Election = module.exports;
+Election.find({/* empty search criteria */}, function(err, estat) {
+    var electionDetails = {
+        status          : "Pre-election"
+    };
+    if (estat) {
+        estat.update(electionDetails, function() { });
+    }
+    else {
+        estat = new Election(electionDetails);
+        estat.save();
+    }
+});
